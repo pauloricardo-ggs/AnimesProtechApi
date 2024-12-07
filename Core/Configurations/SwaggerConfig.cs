c0559c2ef1f5bd3512651619bbf65557e57b7a43
Paulo Ricardo <contact@pauloricardo.dev>
2024-12-06 22:05:22 -0300

    feature(T1): Adição do swagger

---
+++ Core/Configurations/SwaggerConfig.cs
@@ -0,0 +1,18 @@
+namespace Core.Configurations;
+
+public static class SwaggerConfig
+{
+    public static void UseSwaggerIfIsDevelopment(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Animes Protech API"));
+            /// No .net 9 o swagger deixou de ser usado como padrão por falta
+            /// de manutenção e release oficial para o .net 8.
+            /// O recomendado atualmente é o Scalar, que também suporta o OpenApi.
+            /// Para usar o Scalar é só adicionar o pacote Scalar.AspNetCore e
+            /// usar o metodo MapScalarApiReference() ao invés do UseSwaggerUI().
+        }
+    }
+}
