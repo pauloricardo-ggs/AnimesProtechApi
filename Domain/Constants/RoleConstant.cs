0350d28d2afa042f78f6af49c7dd934be4635e60
Paulo Ricardo <contact@pauloricardo.dev>
2024-12-08 00:04:38 -0300

    feature(T9): Autorização via JWT e roles

--- Domain/Constants/RoleConstant.cs
+++ Domain/Constants/RoleConstant.cs
@@ -3,8 +3,5 @@ namespace Domain.Constants;
 public static class RoleConstant
 {
     public const string ADMIN = "admin";
-    public const string USER = "user";
-    
     public const string ADMIN_ID = "4c2c191c-4e20-4a94-83e8-f099bc3c97e5";
-    public const string USER_ID = "56fb0e8e-e16c-494b-b065-4c9d6a91dec2";
 }
